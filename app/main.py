cd98f046b216f5b070c20006d70908fe6f6aa1f3
martaprietos <150271390+martaprietos@users.noreply.github.com>
2026-09-18 10:56:27 +0000

    Lab 1: add user read by ID endpoint

--- app/main.py
+++ app/main.py
@@ -27,3 +27,13 @@ def add_user(new_user: UserCreate):
 @app.get("/api/users")
 def get_users():
    return users
+
+@app.get("/api/users/{user_id}")
+def get_user(user_id: int):
+   for existing_user in users:
+      if existing_user.user_id == user_id:
+         return existing_user
+      raise HTTPException(
+         status_code=status.HTTP_404_NOT_FOUND,
+         detail="User not found",
+        )
