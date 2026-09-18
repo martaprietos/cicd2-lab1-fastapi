938e2ff5732b2231132b0b6671aa6ea24f57cc1f
martaprietos <150271390+martaprietos@users.noreply.github.com>
2026-09-18 10:34:18 +0000

    Added health check route and hello endpoint

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI 
+
+app = FastAPI(title="Lab 1 - FastAPI User API")
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello from FastAPI"}
